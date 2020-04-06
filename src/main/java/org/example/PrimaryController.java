7b345357b3f0a6c590525097b4ffcf88ec4c8f23
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2020-04-07 00:36:13 +0100

    added red circle to mouse pointer click on image

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -6,21 +6,27 @@ import java.io.InputStream;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.*;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class PrimaryController {
 
     public ImageView imageView;
     public Image blackAndWhite;
+    public double xCoord, yCoord;
+    public Pane imagePane;
 
     public void initialize() {
         File file = new File("src/main/resources/org/example/romeMap.jpg");
         Image im = new Image(String.valueOf(file.toURI()), 920, 760, false, false);
         blackAndWhite = setBlackWhite(im);
         imageView.setImage(blackAndWhite);
+        selectWaypoint();
 
     }
 
+    // takes in an image, returns a black and white writable image using RGB values in image, optimised for maps.
     public WritableImage setBlackWhite(Image initialImage) {
         int width = (int) initialImage.getWidth();
         int height = (int) initialImage.getHeight();
@@ -34,7 +40,7 @@ public class PrimaryController {
                 double r = color.getRed() * 255;
                 double g = color.getGreen() * 255;
                 double b = color.getBlue() * 255;
-                if (r > 205 && b > 165 && g > 225){
+                if (r > 205 && b > 165 && g > 225) {
                     pixelWriter.setColor(x, y, Color.WHITE);
                 } else {
                     pixelWriter.setColor(x, y, Color.BLACK);
@@ -42,6 +48,23 @@ public class PrimaryController {
             }
         }
         return writableImage;
+    }
+
+    public void selectWaypoint()    {
+        imagePane.setOnMouseClicked(e -> {
+            imageView.toFront();
+            xCoord = e.getX() - 7;
+            yCoord = e.getY() -7;
+            imagePane.toFront();
+            Circle circle = new Circle();
+            circle.setCenterX(xCoord);
+            circle.setCenterY(yCoord);
+            circle.setRadius(6);
+            circle.setFill(Color.RED);
+           imagePane.getChildren().add(circle);
+           circle.relocate(xCoord,yCoord);
+           System.out.println("xCoord: " + xCoord + ", yCoord: " + yCoord);
+        });
     }
 
 }
